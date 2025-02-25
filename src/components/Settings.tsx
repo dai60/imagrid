e635f8646c7c0709e6d0497b6740843dec7ca387
dai60 <190627676+dai60@users.noreply.github.com>
2025-02-25 13:50:28 +0200

    fix redirect and accessibility

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -6,13 +6,15 @@ type CounterEventHandler = (change: number) => void;
 
 type CounterProps = {
     label: string;
+    addLabel: string;
+    removeLabel: string;
     value: number;
     min: number;
     max: number;
     onChange: CounterEventHandler;
 }
 
-const Counter = ({ label, value, min, max, onChange }: CounterProps) => {
+const Counter = ({ label, addLabel, removeLabel, value, min, max, onChange }: CounterProps) => {
     const id = useId();
 
     const isMin = value === min;
@@ -23,10 +25,10 @@ const Counter = ({ label, value, min, max, onChange }: CounterProps) => {
             <label className="block text-xs mb-1" htmlFor={id}>{label}</label>
             <div className="flex items-stretch w-fit border border-primary rounded-md overflow-hidden">
                 <input className="w-14 px-2 py-1" type="number" id={id} readOnly disabled value={value} min={min} max={max} step={1} />
-                <button className="px-2 py-1 w-8 disabled:bg-disabled cursor-pointer disabled:cursor-default" disabled={isMax} onClick={() => onChange(1)}>
+                <button className="px-2 py-1 w-8 disabled:bg-disabled cursor-pointer disabled:cursor-default" aria-label={addLabel} disabled={isMax} onClick={() => onChange(1)}>
                     <Add />
                 </button>
-                <button className="px-2 py-1 w-8 disabled:bg-disabled cursor-pointer disabled:cursor-default" disabled={isMin} onClick={() => onChange(-1)}>
+                <button className="px-2 py-1 w-8 disabled:bg-disabled cursor-pointer disabled:cursor-default" aria-label={removeLabel} disabled={isMin} onClick={() => onChange(-1)}>
                     <Remove />
                 </button>
             </div>
@@ -63,10 +65,10 @@ const Settings = () => {
     return (
         <>
             <div className="my-2">
-                <Counter label="Grid Columns:" min={1} max={12} value={state.gridCols} onChange={handleGridCols} />
+                <Counter label="Grid Columns:" addLabel="Add column" removeLabel="Remove column" min={1} max={12} value={state.gridCols} onChange={handleGridCols} />
             </div>
             <div className="my-2">
-                <Counter label="Grid Rows:" min={1} max={12} value={state.gridRows} onChange={handleGridRows} />
+                <Counter label="Grid Rows:" addLabel="Add row" removeLabel="Remove row" min={1} max={12} value={state.gridRows} onChange={handleGridRows} />
             </div>
             <div className="my-2">
                 <label htmlFor={elemSizeId} className="block text-xs mb-1">Element size:</label>
